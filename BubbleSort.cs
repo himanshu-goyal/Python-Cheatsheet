3caafa13e67e9e8e2f9eb8abe9b6615ac5b53fd9
himanshu goyal <himanshu.goyal@outlook.com>
2019-07-02 16:51:24 -0500

    Create BubbleSort.cs

---
+++ BubbleSort.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] number = { 89, 76, 45, 92, 67, 12, 99 };
+            bool flag = true;
+            int temp;
+            int numLength = number.Length;
+
+            //sorting an array  
+            for (int i = 1; (i <= (numLength - 1)) && flag; i++)
+            {
+                flag = false;
+                for (int j = 0; j < (numLength - 1); j++)
+                {
+                    if (number[j + 1] > number[j])
+                    {
+                        temp = number[j];
+                        number[j] = number[j + 1];
+                        number[j + 1] = temp;
+                        flag = true;
+                    }
+                }
+            }
+
+            //Sorted array  
+            Console.WriteLine(string.Join(",", number));
+            Console.Read();
+        }
+    }
+}
